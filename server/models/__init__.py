bf68753f791e2aff009aab4e4b8fac4010e37523
CreativeCoder <damariskerry@gmail.com>
2025-03-19 18:49:20 +0300

    Update __init__.py

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,5 +1,5 @@
-    # Import and register models
-    from .drivers import Driver
-    from .trucks import Truck
-    from .assignments import Assignment
-    from .users import User
+# Import and register models
+from .drivers import Driver
+from .trucks import Truck
+from .assignments import Assignment
+from .users import User
